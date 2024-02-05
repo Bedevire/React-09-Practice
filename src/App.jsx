ed82d5516e4b2c10ed4bb2a28bfa2d5162c0a6e6
Adam Pavlik <adam.pavlik@exxeta.com>
2024-02-05 09:28:20 +0100

    Add basic project list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import Projects from './components/Projects'
+import Project from './components/Project'
 
 function App() {
   let projects = [
     {
+      id: 1,
       title: "React",
       tasks: []
     },
     {
+      id: 2,
       title: "Azure",
       tasks: [
         {title: 'AZ-900'},
@@ -15,11 +18,20 @@ function App() {
       ]
     }
   ]
+
+  const [selectedProject, setSelectedProject] = useState(null);
+
+
+  function projectSelected(project){
+    console.log('Project ' + project.title + ' selected');
+    setSelectedProject(project);
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-      <Projects projects={projects} />
-    </> 
+    <main className="h-screen my-8 flex gap-8">
+      <Projects projects={projects} onSelected={projectSelected} />
+      {selectedProject && <Project project={selectedProject} />}
+    </main> 
   );
 }
 
---
+++ src/components/Project.jsx
@@ -0,0 +1,7 @@
+export default function Project({project}){
+    return(
+        <div>
+            <label className="text-sm font-bold uppercase text-stone-500">{project.title}</label>
+        </div>
+    )
+}
